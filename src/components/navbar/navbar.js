5241c04ad08bae8e2c512331bb1e1a1fb4d1a8e5
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-04-21 17:04:45 +0100

    Added the fonts

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -10,6 +10,9 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className={classnames("navbar")}>
+        <h1 className="font-merri">TEAT</h1>
+        <h1 className="font-mont">TEAT</h1>
+
         <a href="#">Item 1</a>
         <a href="#">Item 2</a>
         <a href="#">Item 3</a>
