4744d318a154570197c45459572262a7d2d7ee20
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-04-24 14:33:10 +0100

    Added some test animations

--- src/components/hexa/hexagrid.js
+++ src/components/hexa/hexagrid.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Hexagon from "./hexagon";
+
 import "./hexagon.scss";
 
 export default class HexagonGrid extends Component {
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,28 @@ import SEO from "../components/seo";
 import Navbar from "../components/navbar/navbar";
 import HexagonGrid from "../components/hexa/hexagrid";
 import Hexagon from "../components/hexa/hexagon";
+import anime from "animejs";
 
 import "../styles/tw.css";
 import "../components/hexa/hexagon.scss";
 
 export default class extends Component {
+  componentDidMount() {
+    anime({
+      targets: ".title-text",
+      duration: 1500,
+      easing: "easeInOutExpo",
+      opacity: [0, 1],
+      delay: anime.stagger(75), // increase delay by 100ms for each elements.
+    });
+    anime({
+      targets: ".hexagon",
+      duration: 1500,
+      easing: "easeInOutExpo",
+      opacity: [0, 1],
+      delay: anime.stagger(25), // increase delay by 100ms for each elements.
+    });
+  }
   render() {
     const headerHeight = "95px";
     return (
@@ -30,16 +47,20 @@ export default class extends Component {
           <div className="h-screen w-full bg-white px-3 flex flex-col items-center justify-center">
             <div className="text-start sm:text-center">
               <h4 className="mb-3">
-                <span className="font-mont text-3xl">Hello, I'm </span>
-                <span className="font-merri font-bold text-4xl">
-                  Neelesh 
+                <span className="title-text font-mont text-3xl">
+                  Hello, I'm{" "}
                 </span>
-                <span className="font-mont text-3xl">
+                <span className="title-text font-merri font-bold text-4xl">
+                  Neelesh
+                </span>
+                <span className="title-text font-mont text-3xl">
                   , a web developer.
                 </span>
               </h4>
               <HexagonGrid />
-              <h5 className="font-mont text-3xl my-2">Let's talk?</h5>
+              <h5 className="title-text font-mont text-3xl my-2">
+                Let's talk?
+              </h5>
               <div className="flex flex-row sm:justify-center items-center">
                 <a
                   className="cursor-pointer"
