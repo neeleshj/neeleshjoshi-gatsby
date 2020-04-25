30334afbc993d85d195bb5f1d4cb6b6facece818
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-04-25 10:53:59 +0100

    Updated the square into typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,25 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    +"prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    +"plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  plugins: ["@typescript-eslint", "react"],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
   },
   rules: {
-    "react/prop-types": "off", // Disable prop-types as we use TypeScript for type checking
-    "@typescript-eslint/explicit-function-return-type": "off",
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
-  overrides: [
-    // Override some TypeScript rules just for .js files
-    {
-      files: ["*.js"],
-      rules: {
-        "@typescript-eslint/no-var-requires": "off", //
-      },
+  settings: {
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
     },
-  ],
+  },
 };
---
+++ src/components/square.tsx
@@ -0,0 +1,14 @@
+import React, { Component, FunctionComponent } from "react";
+import classnames from "classnames";
+import "../styles/square.scss";
+
+type SquareProps = {
+  colorClass: string;
+};
+
+export const Square: FunctionComponent<SquareProps> = ({
+  colorClass,
+  children,
+}) => {
+  return <div className={classnames("square", colorClass)}>{children}</div>;
+};
